b5e1fffdc5023c84fbd02bcfe6156e2d2e1464c8
Russell Currie <rcurr@users.noreply.github.com>
2016-03-13 00:12:01 +0000

    Altering version format to abbreviated commit hash

--- ppnc.py
+++ ppnc.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with ppnc.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "$Id$"
+__version__ = "$Format:%h$"
 
 import os
 import datetime
